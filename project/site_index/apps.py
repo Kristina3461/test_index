d8dfa8d011099c3ee779cdd7101ae2ddbe538352
Kristina3461 <117187975+Kristina3461@users.noreply.github.com>
2023-10-16 21:37:35 +0700

    Delete project directory

--- project/site_index/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SiteIndexConfig(AppConfig):
-    name = 'site_index'
--- project/site_index/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import site_index_view
-
-urlpatterns = [
-    path('', site_index_view, name='site_index'),
-]
